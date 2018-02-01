b7c31c1bb08dba2c5944535e8677a182701a68f3
bianca <bianca@nic.br>
2018-02-01 16:27:15 -0200

    Curso feito até o capítulo 3

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo" >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  
+  data() {
+    return {
+      titulo: 'Alurapic',
+      fotos: [
+        {
+          url: 'http://tudosobrecachorros.com.br/wp-content/uploads/cachorro-independente.jpg',
+          titulo: 'Cachorro'
+        },
+        {
+          url: 'http://tudosobrecachorros.com.br/wp-content/uploads/cachorro-independente.jpg',
+          titulo: 'Cachorrão'
+        }]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
